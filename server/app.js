be20fb4f3bb2d73b2253a876ec84655be81ce4c3
Vikas Kumar <vikasjson@gmail.com>
2018-09-07 21:33:29 +0530

    feat(Config) : Editor config added

--- server/app.js
+++ server/app.js
@@ -1,12 +1,16 @@
 
 const http = require("http");
+const express = require('express');
+const app = express();
 
-const app = http.createServer((req, res) =>{
+const server = http.createServer((req, res) => {
 
     res.writeHead(200, {'Content-Type': 'text/json'});
     res.end('\n Hello World ! \n');
 
 }).listen(8060);
 
-console.log('Welcome to nodejs application');
+console.log('Server is running on port number- 8060');
+
+module.exports = server;
 
